bda0b8ca1e3be1b49fb0e497f94f7eeca610bc98
kicconsu <sicconsu009@gmail.com>
2024-10-21 09:10:33 -0500

    some graphic effect fixes

--- res/scripts/3d_charge.gd
+++ res/scripts/3d_charge.gd
@@ -4,6 +4,7 @@ extends Node3D
 @export var type:int = 0
 @export var char:float = 1
 @export var radius:float = 2
+@export_range(16, 255) var transparency:int = 255
 @onready var mesh:CSGMesh3D = $CSGMesh3D
 
 func _ready() -> void:
@@ -12,15 +13,16 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	
 	if(self.char > 0):
-		self.mesh.material.next_pass.albedo_color = '#ff2165'
+		self.mesh.material.albedo_color = '#ff2165' + ('%x' % self.transparency)
 	else:
-		self.mesh.material.next_pass.albedo_color = '#5087ff'
+		self.mesh.material.albedo_color = '#5087ff' + ('%x' % self.transparency)
 		
 	if(self.type == 0):
 		if(not is_instance_of(mesh.mesh, SphereMesh)):
 			mesh.set_mesh(SphereMesh.new())
 			mesh.mesh.radial_segments = 10
 			mesh.mesh.rings = 5
+			mesh.material.set_cull_mode(0)
 		mesh.mesh.radius = 0.3
 		mesh.mesh.height = 0.3*2
 	elif(self.type == 1):
@@ -28,7 +30,8 @@ func _process(_delta: float) -> void:
 			mesh.set_mesh(SphereMesh.new())
 			mesh.mesh.radial_segments = 10
 			mesh.mesh.rings = 5
-		mesh.mesh.radius = self.radius/10
+			mesh.material.set_cull_mode(0)
+		mesh.mesh.radius = self.radius
 		mesh.mesh.height = mesh.mesh.radius*2
 	elif(self.type == 2):
 		if(not is_instance_of(mesh.mesh, CylinderMesh)):
@@ -37,6 +40,7 @@ func _process(_delta: float) -> void:
 			mesh.mesh.cap_top = false
 			mesh.mesh.cap_bottom = false
 			mesh.mesh.radial_segments = 5
+			mesh.material.set_cull_mode(0)
 		mesh.mesh.top_radius = 0.1
 		mesh.mesh.bottom_radius = 0.1
 	elif(self.type == 3):
@@ -46,6 +50,7 @@ func _process(_delta: float) -> void:
 			mesh.mesh.cap_top = false
 			mesh.mesh.cap_bottom = false
 			mesh.mesh.radial_segments = 5
+			mesh.material.set_cull_mode(0)
 		mesh.mesh.top_radius = self.radius*3
 		mesh.mesh.bottom_radius = self.radius*3
 	elif(self.type == 4):
@@ -53,4 +58,5 @@ func _process(_delta: float) -> void:
 			mesh.set_mesh(PlaneMesh.new())
 			mesh.mesh.size.x = 10
 			mesh.mesh.size.y = 10
+			mesh.material.set_cull_mode(2)
 		
