5c949da1fdf785be2e46387a48e99b1bb157ec90
kicconsu <sicconsu009@gmail.com>
2024-10-20 23:00:42 -0500

    3d dynamic meshes implemented

--- res/scripts/3d_charge.gd
+++ res/scripts/3d_charge.gd
@@ -1,5 +1,56 @@
+@tool
 extends Node3D
 
 @export var type:int = 0
-@export var char:float = 5
+@export var char:float = 1
 @export var radius:float = 2
+@onready var mesh:CSGMesh3D = $CSGMesh3D
+
+func _ready() -> void:
+	self.mesh.material = self.mesh.material.duplicate(true)
+
+func _process(_delta: float) -> void:
+	
+	if(self.char > 0):
+		self.mesh.material.next_pass.albedo_color = '#ff2165'
+	else:
+		self.mesh.material.next_pass.albedo_color = '#5087ff'
+		
+	if(self.type == 0):
+		if(not is_instance_of(mesh.mesh, SphereMesh)):
+			mesh.set_mesh(SphereMesh.new())
+			mesh.mesh.radial_segments = 10
+			mesh.mesh.rings = 5
+		mesh.mesh.radius = 0.3
+		mesh.mesh.height = 0.3*2
+	elif(self.type == 1):
+		if(not is_instance_of(mesh.mesh, SphereMesh)):
+			mesh.set_mesh(SphereMesh.new())
+			mesh.mesh.radial_segments = 10
+			mesh.mesh.rings = 5
+		mesh.mesh.radius = self.radius/10
+		mesh.mesh.height = mesh.mesh.radius*2
+	elif(self.type == 2):
+		if(not is_instance_of(mesh.mesh, CylinderMesh)):
+			mesh.set_mesh(CylinderMesh.new())
+			mesh.mesh.height = 1000
+			mesh.mesh.cap_top = false
+			mesh.mesh.cap_bottom = false
+			mesh.mesh.radial_segments = 5
+		mesh.mesh.top_radius = 0.1
+		mesh.mesh.bottom_radius = 0.1
+	elif(self.type == 3):
+		if(not is_instance_of(mesh.mesh, CylinderMesh)):
+			mesh.set_mesh(CylinderMesh.new())
+			mesh.mesh.height = 1000
+			mesh.mesh.cap_top = false
+			mesh.mesh.cap_bottom = false
+			mesh.mesh.radial_segments = 5
+		mesh.mesh.top_radius = self.radius*3
+		mesh.mesh.bottom_radius = self.radius*3
+	elif(self.type == 4):
+		if(not is_instance_of(mesh.mesh, PlaneMesh)):
+			mesh.set_mesh(PlaneMesh.new())
+			mesh.mesh.size.x = 10
+			mesh.mesh.size.y = 10
+		
--- res/scripts/3d_scene_handler.gd
+++ res/scripts/3d_scene_handler.gd
@@ -149,7 +149,7 @@ func _charges_uniform_update() -> RDUniform:
 		# 12 bytes + 4 packing = 16 - at 0
 		chargeBytes.append_array(PackedFloat32Array([chargeTransform.origin.x, chargeTransform.origin.y, chargeTransform.origin.z, -1]).to_byte_array())
 		# 16 bytes - at 16
-		chargeBytes.append_array(PackedFloat32Array([chargeTransform.basis.z.x, chargeTransform.basis.z.y, chargeTransform.basis.z.z, charge.radius]).to_byte_array())
+		chargeBytes.append_array(PackedFloat32Array([chargeTransform.basis.y.x, chargeTransform.basis.y.y, chargeTransform.basis.y.z, charge.radius]).to_byte_array())
 		# 4 bytes - at 32
 		chargeBytes.append_array(PackedFloat32Array([charge.char]).to_byte_array())
 		# 4 bytes - at 36 -> add 4*2 padding to end with a size of 48
