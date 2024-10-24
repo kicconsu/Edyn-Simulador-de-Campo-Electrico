4fb192b68cc075c47a9d5e7c9fc55610d538984d
kicconsu <sicconsu009@gmail.com>
2024-10-24 16:09:28 -0500

    silly uniform bugfix

--- res/scripts/3d_scene_handler.gd
+++ res/scripts/3d_scene_handler.gd
@@ -45,7 +45,7 @@ func _ready() -> void:
 	#TODO: make it so offset_vectors just swaps out the offset_img in the container. let process() do the rest.
 
 
-func _input(event):
+func _input(_event):
 	
 	if Input.is_action_just_pressed("changeProjection"):
 		if changeProjection:
@@ -65,7 +65,7 @@ func _input(event):
 			toggle = !toggle
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	self.refresh_uniforms()
 	ecamp = render_ecamp()
 	$"3dContainer".offset_vectors(ecamp)
@@ -176,18 +176,21 @@ func _charges_uniform_update() -> RDUniform:
 	var chargeBytes := PackedByteArray()
 	#add each charge's data to the array
 	var c:int = 0
-	for charge in charges:
-		var chargeTransform:Transform3D = charge.get_global_transform()
-		# 12 bytes + 4 packing = 16 - at 0
-		chargeBytes.append_array(PackedFloat32Array([chargeTransform.origin.x, chargeTransform.origin.y, chargeTransform.origin.z, -1]).to_byte_array())
-		# 16 bytes - at 16
-		chargeBytes.append_array(PackedFloat32Array([chargeTransform.basis.y.x, chargeTransform.basis.y.y, chargeTransform.basis.y.z, charge.radius]).to_byte_array())
-		# 4 bytes - at 32
-		chargeBytes.append_array(PackedFloat32Array([charge.char]).to_byte_array())
-		# 4 bytes - at 36 -> add 4*2 padding to end with a size of 48
-		chargeBytes.append_array(PackedInt32Array([charge.type, -1, -1]).to_byte_array())
-		#print("charge, ", c, " charge: ", charge.char, " type: ", charge.type)
-		c += 1
+	if charges.is_empty():
+		chargeBytes.append_array(PackedFloat32Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).to_byte_array())
+	else:
+		for charge in charges:
+			var chargeTransform:Transform3D = charge.get_global_transform()
+			# 12 bytes + 4 packing = 16 - at 0
+			chargeBytes.append_array(PackedFloat32Array([chargeTransform.origin.x, chargeTransform.origin.y, chargeTransform.origin.z, -1]).to_byte_array())
+			# 16 bytes - at 16
+			chargeBytes.append_array(PackedFloat32Array([chargeTransform.basis.y.x, chargeTransform.basis.y.y, chargeTransform.basis.y.z, charge.radius]).to_byte_array())
+			# 4 bytes - at 32
+			chargeBytes.append_array(PackedFloat32Array([charge.char]).to_byte_array())
+			# 4 bytes - at 36 -> add 4*2 padding to end with a size of 48
+			chargeBytes.append_array(PackedInt32Array([charge.type, -1, -1]).to_byte_array())
+			#print("charge, ", c, " charge: ", charge.char, " type: ", charge.type)
+			c += 1
 	#Uniform setup
 	var charBuffer := rd.storage_buffer_create(chargeBytes.size(), chargeBytes)
 	var charUniform := RDUniform.new()
